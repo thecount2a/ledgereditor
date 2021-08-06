1fa36bc9bc81d15ccc0b3412f613d2c35b3eeb5b
Garret McGraw <jetaimed@gmail.com>
2021-08-06 11:03:10 -0400

    Committing stuff for smaller screens

--- src/components/LedgerBlock.vue
+++ src/components/LedgerBlock.vue
@@ -2,7 +2,7 @@
     <v-card v-if="(lblock.type == 'other' && lblock.text != '') || lblock.type != 'other'">
             <v-container>
                 <v-row no-gutters>
-                    <v-col cols="1" v-if="lblock.type == 'transaction'">
+                    <v-col md="1" v-if="lblock.type == 'transaction'">
                         <v-menu
                             v-model="datemenu"
                             :close-on-content-click="false"
@@ -29,27 +29,27 @@
                             ></v-date-picker>
                         </v-menu>
                     </v-col>
-                    <v-col cols="1" v-if="lblock.type == 'transaction'">
+                    <v-col md="1" v-if="lblock.type == 'transaction'">
                         <v-select v-if="!showcodeinsteadofstatus" hide-details="auto" filled v-model="lblock.status" :items="['', '!', '*']" label="Status" :disabled="onlyenable.length > 0 && !onlyenable.includes('Status')"></v-select>
                         <v-text-field v-else hide-details="auto" filled label="Code" v-model="lblock.code" :disabled="onlyenable.length > 0 && !onlyenable.includes('Code')"></v-text-field>
                     </v-col>
-                    <v-col :cols="(open || alwaysshowcomments) ? 4 : 2" v-if="lblock.type == 'transaction'">
+                    <v-col :md="(open || alwaysshowcomments) ? 4 : 2" cols="12" v-if="lblock.type == 'transaction'">
                         <v-text-field hide-details="auto" filled label="Description" v-model="lblock.description" :disabled="onlyenable.length > 0 && !onlyenable.includes('Description')"></v-text-field>
                     </v-col>
-                    <v-col cols="6" v-if="(open || alwaysshowcomments) && lblock.type == 'transaction'">
+                    <v-col md="6" cols="12" v-if="(open || alwaysshowcomments) && lblock.type == 'transaction'">
                         <v-text-field hide-details="auto" filled label="Comment" v-model="lblock.comment" :disabled="onlyenable.length > 0 && !onlyenable.includes('Comment')"></v-text-field>
                     </v-col>
-                    <v-col :cols="open || (lblock.type =='other' && lblock.postingIndexes.length <=0) ? 12 : 4" v-if="lblock.type == 'other'">
+                    <v-col :md="open || (lblock.type =='other' && lblock.postingIndexes.length <=0) ? 12 : 4" v-if="lblock.type == 'other'">
                         <v-textarea hide-details="auto" filled auto-grow label="Generic Ledger Block" rows="1" v-model="lblock.text"></v-textarea>
                     </v-col>
-                    <v-col cols="12" v-if="lblock.type == 'comment'">
+                    <v-col md="12" v-if="lblock.type == 'comment'">
                         <v-textarea hide-details="auto" filled auto-grow label="Comment Block" rows="3" v-model="lblock.text"></v-textarea>
                     </v-col>
-                    <v-col v-if="(lblock.type == 'transaction' && !open && alwaysshowcomments)" cols="4">&nbsp;</v-col>
-                    <v-col cols="3" v-if="!open && lblock.type != 'comment' && (lblock.postingIndexes.length > 0 || lblock.type =='transaction')">
+                    <v-col v-if="(lblock.type == 'transaction' && !open && alwaysshowcomments)" md="4" cols="0">&nbsp;</v-col>
+                    <v-col md="3" cols="12" v-if="!open && lblock.type != 'comment' && (lblock.postingIndexes.length > 0 || lblock.type =='transaction')">
                         <v-combobox hide-details="auto" :filled="alwaysshowcomments?false:true" v-model="firstAccount" :items="accounts" label="First Account" :disabled="onlyenable.length > 0 && !onlyenable.includes('Account')"></v-combobox>
                     </v-col>
-                    <v-col cols="2" v-if="!open && lblock.type != 'comment' && (lblock.postingIndexes.length > 0 || lblock.type =='transaction')">
+                    <v-col md="2" cols="12" v-if="!open && lblock.type != 'comment' && (lblock.postingIndexes.length > 0 || lblock.type =='transaction')">
                         <v-text-field hide-details="auto" :filled="alwaysshowcomments?false:true" label="Amount" v-model="firstAmount" :disabled="onlyenable.length > 0 && !onlyenable.includes('Amount')">
                             <v-icon slot="prepend" :color="firstAmount.indexOf('-')>=0?'red':'black'">
                                 {{ firstAmount.indexOf('-')>=0?'mdi-arrow-right-bold':'mdi-arrow-left-bold' }}
@@ -59,12 +59,14 @@
                             </v-icon>
                         </v-text-field>
                     </v-col>
-                    <v-col cols="3" v-if="!open && lblock.type != 'comment' && (lblock.postingIndexes.length > 0 || lblock.type =='transaction')">
+                    <v-col md="3" cols="12" v-if="!open && lblock.type != 'comment' && (lblock.postingIndexes.length > 0 || lblock.type =='transaction')">
                         <v-combobox hide-details="auto" :filled="alwaysshowcomments?false:true" v-model="secondAccount" :items="accounts" label="Second Account" :disabled="lblock.postingIndexes.length > 2 || (onlyenable.length > 0 && !onlyenable.includes('Second Account'))"></v-combobox>
                     </v-col>
+                    <v-col md="0" cols="12" v-if="open" class="md12"></v-col>
+                    <v-col md="0" cols="12" v-if="open"></v-col>
                 </v-row>
                 <v-row v-if="lblock.type != 'comment'" no-gutters justify="end">
-                    <v-col cols="12" align="end">
+                    <v-col md="12" align="end">
                         <v-icon v-if="open" @click="addPosting()" :tabindex="onlyenable.length > 0 ? -1 : undefined" color="green">mdi-plus-thick</v-icon>
                         <v-menu offset-y>
                             <template v-slot:activator="{ on, attrs }">
@@ -84,13 +86,13 @@
                         <div v-for="(lline,lineIndex) in lblock.lines">
                             <v-row v-if="lline.type=='posting'" no-gutters>
                                 <v-spacer></v-spacer>
-                                <v-col cols="3">
+                                <v-col md="3" sm="4" cols="6">
                                     <v-combobox hide-details="auto" v-model="lline.account" :items="accounts" label="Account" :disabled="onlyenable.length > 0 && !onlyenable.includes('Account')"></v-combobox>
                                 </v-col>
-                                <v-col cols="2">
+                                <v-col md="2" sm="3" cols="6">
                                     <v-text-field hide-details="auto" :label="'Amount'+(lline.amount == '' ? computedAmountInfo : '')" v-model="lline.amount" :disabled="onlyenable.length > 0 && !onlyenable.includes('Amount')"></v-text-field>
                                 </v-col>
-                                <v-col cols="4">
+                                <v-col md="4" cols="12">
                                     <v-text-field hide-details="auto" label="Comment" v-model="lline.comment" :disabled="onlyenable.length > 0 && !onlyenable.includes('Comment')">
                                         <v-icon @click="deletePosting(lineIndex)" slot="append-outer" color="red">mdi-delete</v-icon>
                                     </v-text-field>
